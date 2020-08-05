84439d4c1e32473bde89ba2ee170cc1a13a4c4b7
mostafakhalaf <mostafakhalafmohamed@gmail.com>
2020-08-05 10:29:13 +0200

    Add files via upload

---
+++ core/ViewModel/RegisterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MostafaKhalafFullStack.ViewModel
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string FullName { get; set; }
+        public string Password { get; set; }
+        public int Age { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Gender { get; set; }
+        [EmailAddress]
+        
+        public string Email { get; set; }
+
+
+    }
+}
