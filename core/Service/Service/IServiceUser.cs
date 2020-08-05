c4042f129d045604a69d2194d885160ca279cf11
mostafakhalaf <mostafakhalafmohamed@gmail.com>
2020-08-05 10:28:11 +0200

    Add files via upload

---
+++ core/Service/Service/IServiceUser.cs
@@ -0,0 +1,18 @@
+﻿using MostafaKhalafFullStack.Model;
+using MostafaKhalafFullStack.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MostafaKhalafFullStack.Service.Iservice
+{
+    public interface IServiceUser
+    {
+         void PostUser(ApplicationUser model);
+        IEnumerable<ApplicationUser> GetAllUser();
+        void EditUser(ApplicationUser model);
+        void DeleteUser(ApplicationUser model);
+        ApplicationUser GetByIDUser(object id);
+    }
+}
