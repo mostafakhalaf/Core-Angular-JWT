2ca69fe00c33f258b44de2bdbb957c60b1757f80
mostafakhalaf <mostafakhalafmohamed@gmail.com>
2020-08-05 10:22:04 +0200

    Add files via upload

---
+++ core/Repository/Generic/IGRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MostafaKhalafFullStack.Repository.Generic
+{
+   public interface IGRepository<T> where T : class
+    {
+
+        void Add(T entity);
+        void  Remove(T entity);
+        IEnumerable<T> GetAll();
+        T Find(params object[] keys);
+        void AddRange(IEnumerable<T> entities);
+        void Update(T entity);
+        T GetFirst();
+    }
+}
---
+++ core/Repository/Generic/Repository.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using MostafaKhalafFullStack.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MostafaKhalafFullStack.Repository.Generic
+{
+    public class Repository<T> : IGRepository<T> where T : class
+    {
+        protected readonly AppDbContext dbContext;
+        public Repository(AppDbContext dbContext)
+        {
+
+            this.dbContext = dbContext;
+        }
+        public void Add(T entity)
+        {
+
+             dbContext.Set<object>().Add(entity);
+        }
+
+        public virtual IEnumerable<T> GetAll()
+        {
+            return dbContext.Set<T>().AsNoTracking().ToList<T>();
+        }
+
+        public virtual void AddRange(IEnumerable<T> entities)
+        {
+            dbContext.Set<T>().AddRange(entities);
+        }
+
+        public virtual T GetFirst()
+        {
+            return dbContext.Set<T>().AsNoTracking().FirstOrDefault();
+        }
+        public T Find(params object[] keys)
+        {
+            var m = dbContext.Set<T>().Find(keys);
+            return dbContext.Set<T>().Find(keys);
+        }
+
+        public void Remove(T entity)
+        {
+             dbContext.Remove(entity);
+        }
+
+        public void Update(T entity)
+        {
+            foreach (var entry in dbContext.ChangeTracker.Entries<T>().ToList())
+            {
+                entry.State = EntityState.Detached;
+            }
+             dbContext.Update(entity);
+        }
+    }
+}
