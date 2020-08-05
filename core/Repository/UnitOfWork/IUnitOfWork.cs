d849d3b1bf2e9ccf6c3df9f43c5f852be386dff8
mostafakhalaf <mostafakhalafmohamed@gmail.com>
2020-08-05 10:25:46 +0200

    Add files via upload

---
+++ core/Repository/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MostafaKhalafFullStack.Repository.UnitOfWork
+{
+   public interface IUnitOfWork
+    {
+       void Complete();
+    }
+}
---
+++ core/Repository/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using MostafaKhalafFullStack.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MostafaKhalafFullStack.Repository.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+
+    {
+        private readonly AppDbContext Context;
+        public UnitOfWork(AppDbContext Context)
+        {
+            this.Context = Context;
+        }
+        public void Complete()
+        {
+            Context.SaveChanges();
+        }
+    }
+}
